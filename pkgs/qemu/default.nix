85f8b6710d4ce1c7ecfd6487c3d62149426e43b4
Tinghan Zhu <tinghan.zhu@hotmail.com>
2026-04-28 08:46:59 -0500

    update qemu patch

--- pkgs/qemu/default.nix
+++ pkgs/qemu/default.nix
@@ -42,50 +42,6 @@ in
 
   patches = (old.patches or []) ++ [ patchFile ];
 
-  postPatch = (old.postPatch or "") + ''
-    # spoof_acpi: ACPI OEM identifiers
-    sed -i \
-      -e 's/\(#define ACPI_BUILD_APPNAME6 \)"[^"]*"/\1"${acpiOemId}"/' \
-      -e 's/\(#define ACPI_BUILD_APPNAME8 \)"[^"]*"/\1"${acpiOemTableId}"/' \
-      include/hw/acpi/aml-build.h
-
-    sed -i 's/"ACPI"/"${acpiCreatorId}"/g' hw/acpi/aml-build.c
-
-    ${lib.optionalString (acpiPmProfile == 2) ''
-      sed -i 's/1 \/\* Desktop \*\/, 1/2 \/* Mobile *\/, 1/' hw/acpi/aml-build.c
-    ''}
-
-    # spoof_smbios: processor manufacturer
-    sed -i \
-      "s/smbios_set_defaults(\"[^\"]*\",/smbios_set_defaults(\"${smbiosManufacturer}\",/" \
-      hw/i386/fw_cfg.c
-
-    # spoof_models: drive model strings
-    ${lib.optionalString spoofModels ''
-      sed -i -E \
-        -e 's/"HL-DT-ST BD-RE WH16NS60"/"${selectedCdModel}"/' \
-        -e 's/"Hitachi HMS360404D5CF00"/"${selectedCfataModel}"/' \
-        -e 's/"Samsung SSD 980 500GB"/"${selectedIdeModel}"/' \
-        hw/ide/core.c
-
-      sed -i -E \
-        's/"NVMe Ctrl"/"${selectedNvmeModel}"/' \
-        hw/nvme/ctrl.c
-    ''}
-
-    # spoof_serials: randomize USB device serial strings
-    ${lib.optionalString spoofUsbSerials ''
-      for f in hw/usb/*.c; do
-        for pat in STRING_SERIALNUMBER STR_SERIALNUMBER STR_SERIAL_MOUSE STR_SERIAL_TABLET STR_SERIAL_KEYBOARD STR_SERIAL_COMPAT; do
-          while IFS= read -r lineno; do
-            serial=$(head -c 10 /dev/urandom | od -An -tx1 | tr -d ' \n' | head -c 10 | tr 'a-f' 'A-F')
-            sed -r -i "''${lineno}s/(\[\s*$pat\s*\]\s*=\s*\")[^\"]*(\")/\1$serial\2/" "$f"
-          done < <(grep -n "$pat" "$f" | grep -oP '^\d+')
-        done
-      done
-    ''}
-  '';
-
   postInstall = (old.postInstall or "") + ''
     ln -sf $out/bin/qemu-system-x86_64 $out/bin/qemu-kvm
   '';
